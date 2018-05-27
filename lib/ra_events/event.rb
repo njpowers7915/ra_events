fd63fdee316724080a1d13bf90aa2c7f3599ca99
Nicholas Powers <njpowers7915@gmail.com>
2018-05-27 20:06:48 +0000

    Creating an initialize method for event

--- lib/ra_events/event.rb
+++ lib/ra_events/event.rb
@@ -4,32 +4,34 @@ class RaEvents::Event
   
   @@all = []
   
-  def initialize
+  def self.new_from_url # will need url as argument
+    self.new("Event name", "Event Date", "City")
+  
+  def initialize(name=nil, date=nil, city=nil)
+    @name = name
+    @date = date
+    @city = city
     @@all << self
   end
   
-  def name
-  end
-  
-  def date
-  end
-  
-  def city
-  end
-  
   def url
+    @url
   end
   
   def venue
+    @venue
   end
   
   def ticket_url
+    @ticket_url
   end
   
   def cost
+    @cost
   end
   
   def min_age
+    @min_age
   end
   
 end
