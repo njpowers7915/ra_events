346ac05af9f01ad29033c3db5ebf504a2bbbbdf9
Nicholas Powers <njpowers7915@gmail.com>
2018-05-28 02:43:23 +0000

    Created #get_page method in scraper class that makes use of url_creator method from CLI class

--- lib/ra_events/scraper.rb
+++ lib/ra_events/scraper.rb
@@ -2,6 +2,11 @@ class RaEvents::Scraper
   #Using EVENT_ARRAY as hardcoded test array.
   EVENT_ARRAY = [["name1", "date1", "city1"], ["name2", "date2", "city2"]]
   
+ # def get_page
+#    url = url_creator(state)
+ #   Nokogiri::HTML(open(url))
+#  end
+  
   def make_events
     #Takes an array of scraped events. For each event in the array, a new Event object is created.
     EVENT_ARRAY.each do |i|
